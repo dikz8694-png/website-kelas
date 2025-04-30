c34a51a0b320482e1d2c14a8ecff2eeb4daaf328
iball30 <118620952+iball30@users.noreply.github.com>
2025-04-30 16:39:26 +0700

    Create Sabtu.jsx

---
+++ src/components/Mapel/Sabtu.jsx
@@ -0,0 +1,38 @@
+const Sabtu = () => {
+    return (
+        <>
+        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Pkwu</div>
+					<div className="flex justify-center items-center text-sm">08.00-09.20</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Sejarah Peminatan</div>
+					<div className="flex justify-center items-center text-sm">09.20-10.00</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
+					<div className="flex justify-center items-center text-sm">10.15-10.55</div>
+				</div>
+
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
+					<div className="flex justify-center items-center text-sm">10.55-12.15</div>
+				</div>
+				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">12.15-12.45</div>
+				</div>
+				
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1100">
+					<div className="w-[50%] text-base  font-medium">PKN</div>
+					<div className="flex justify-center items-center text-sm">12.45-14.05</div>
+				</div>
+        </>
+    )
+} 
+
+export default Sabtu;
