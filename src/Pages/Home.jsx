158cba8547e5da9723fa7826e80a686e35d9e82b
iball30 <118620952+iball30@users.noreply.github.com>
2025-04-30 12:39:30 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,18 +13,18 @@ const Home = () => {
 					className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative"
 					id="TotalSiswa">
 					<div id="AngkaGradientBlue" className="flex items-center justify-center ">
-						16
+						12
 					</div>
 					<div className="">
 						<span id="AngkaGradientBlue" className="text-5xl">
 							3
 						</span>
 						<span id="AngkaGradientPink" className="text-5xl">
-							4
+							6
 						</span>
 					</div>
 					<div id="AngkaGradientPink" className="flex items-center justify-center">
-						18
+						24
 					</div>
 				</div>
 
