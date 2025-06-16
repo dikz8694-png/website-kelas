c8add9ef792623ca8e0e9d912ddb7ed3870a3882
Iballlajaa <118620952+iball30@users.noreply.github.com>
2025-06-17 01:06:23 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://iballofc.vercel.app">
+			<a href="https://iball.my.id">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
