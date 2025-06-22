53d39ece58deebfe83af137b1892ef4363cee54c
Iballlajaa <118620952+iball30@users.noreply.github.com>
2025-06-22 09:27:25 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -7,7 +7,7 @@ const BoxOldWeb = () => {
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
 				</div>
-				<h1 className="text-white text-base font-semibold pr-0 mt-5">Atmin🤓</h1>
+				<h1 className="text-white text-base font-semibold pr-0 mt-5">Atmin</h1>
 			</a>
 		</div>
 	)
