597aae415c287a65e0bcc35bd8585b6488ec093c
Iball <118620952+iball30@users.noreply.github.com>
2025-07-29 20:59:33 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,31 +2,23 @@ const Selasa = () => {
     return (
         <>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">Sejarah Indonesia</div>
+                <div className="w-full text-base font-medium">Sejarah Peminatan</div>
             </div>
 
             <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">Ekonomi</div>
+                <div className="w-full text-base font-medium">Matematika</div>
             </div>
 
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-                <div className="w-full text-base font-medium">Istirahat</div>
+                <div className="w-full text-base font-medium">B.Indonesia</div>
             </div>
 
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-                <div className="w-full text-base font-medium">B.Inggris</div>
+                <div className="w-full text-base font-medium">Ekonomi</div>
             </div>
 
             <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-                <div className="w-full text-base font-medium">B.Indonesia</div>
-            </div>
-
-            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-                <div className="w-full text-base font-medium">Istirahat</div>
-            </div>
-
-            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1100">
-                <div className="w-full text-base font-medium">B.Indonesia</div>
+                <div className="w-full text-base font-medium">B.Inggris</div>
             </div>
         </>
     )
