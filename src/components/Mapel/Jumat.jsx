1ccf477dfde1439ffefb19f78db3128e01e2572b
Iball <118620952+iball30@users.noreply.github.com>
2025-07-29 21:13:16 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -1,24 +1,15 @@
 const Jumat = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Matematika</div>
-					<div className="flex justify-center items-center text-sm">08.00-09.20</div>
-				</div>
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
-					<div className="flex justify-center items-center text-sm">09.20-10.00</div>
-				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
-				</div>
-				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
-					<div className="flex justify-center items-center text-sm">10.15-11.35</div>
-				</div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">PJOK</div>
+            </div>
+
+            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">PAI</div>
+            </div>
         </>
     )
-} 
+}
 
 export default Jumat;
