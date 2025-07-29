33b57c5d22200e009ff390b7613c535935195bc4
Iball <118620952+iball30@users.noreply.github.com>
2025-07-29 21:06:26 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -1,31 +1,27 @@
 const Rabu = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">PJOK</div>
-					<div className="flex justify-center items-center text-sm">08.00-10.00</div>
-				</div>
-		
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
-				</div>
-				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">Pend.Agama</div>
-					<div className="flex justify-center items-center text-sm">10.15-12.15</div>
-				</div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">Sejarah Indonesia</div>
+            </div>
 
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">12.15-12.45</div>
-				</div>
-				
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Geografi</div>
-					<div className="flex justify-center items-center text-sm">12.45-14.05</div>
-				</div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">B.Inggris LM</div>
+            </div>
+
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-full text-base font-medium">Sosiologi</div>
+            </div>
+
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+                <div className="w-full text-base font-medium">Pkwu</div>
+            </div>
+
+            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+                <div className="w-full text-base font-medium">Geografi</div>
+            </div>
         </>
     )
-} 
+}
 
 export default Rabu;
