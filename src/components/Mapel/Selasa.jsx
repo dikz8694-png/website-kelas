1030ad1858ad84f63629626198fd550e9fc8c2d2
Iball <118620952+iball30@users.noreply.github.com>
2025-07-29 20:57:29 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -1,38 +1,35 @@
 const Selasa = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Sejarah Indonesia</div>
-					<div className="flex justify-center items-center text-sm">08.00-09.20</div>
-				</div>
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
-					<div className="flex justify-center items-center text-sm">09.20-10.00</div>
-				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
-				</div>
-				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">B.Inggris</div>
-					<div className="flex justify-center items-center text-sm">10.15-11.35</div>
-				</div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">Sejarah Indonesia</div>
+            </div>
 
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
-					<div className="flex justify-center items-center text-sm">11.35-12.15</div>
-				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">12.15-12.45</div>
-				</div>
-				
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1100">
-					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
-					<div className="flex justify-center items-center text-sm">12.45-14.05</div>
-				</div>
+            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-full text-base font-medium">Ekonomi</div>
+            </div>
+
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-full text-base font-medium">Istirahat</div>
+            </div>
+
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+                <div className="w-full text-base font-medium">B.Inggris</div>
+            </div>
+
+            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+                <div className="w-full text-base font-medium">B.Indonesia</div>
+            </div>
+
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+                <div className="w-full text-base font-medium">Istirahat</div>
+            </div>
+
+            <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1100">
+                <div className="w-full text-base font-medium">B.Indonesia</div>
+            </div>
         </>
     )
-} 
+}
 
 export default Selasa;
