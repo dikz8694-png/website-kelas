9caac27788625a456b367e59a5fdfb860308020a
dikz8694-png <dikz8694@gmail.com>
2025-08-01 20:38:05 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,11 +2,11 @@ const Selasa = () => {
     return (
         <>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">Sejarah Peminatan</div>
+                <div className="w-full text-base font-medium">Ipa</div>
             </div>
 
             <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">Matematika</div>
+                <div className="w-full text-base font-medium">Akidah Akhlak</div>
             </div>
 
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,11 +14,11 @@ const Selasa = () => {
             </div>
 
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-                <div className="w-full text-base font-medium">Ekonomi</div>
+                <div className="w-full text-base font-medium">Matematika</div>
             </div>
 
             <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-                <div className="w-full text-base font-medium">B.Inggris</div>
+                <div className="w-full text-base font-medium">Al-Qur'an hadis</div>
             </div>
         </>
     )
