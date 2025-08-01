d85d24f2ef832e32efabbb8dd53aed681610b42b
dikz8694-png <dikz8694@gmail.com>
2025-08-01 20:35:08 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -2,23 +2,23 @@ const Rabu = () => {
     return (
         <>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">Sejarah Indonesia</div>
+                <div className="w-full text-base font-medium">Bk</div>
             </div>
 
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-full text-base font-medium">B.Inggris LM</div>
+                <div className="w-full text-base font-medium">Ppkn</div>
             </div>
 
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-                <div className="w-full text-base font-medium">Sosiologi</div>
+                <div className="w-full text-base font-medium">Ips</div>
             </div>
 
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-                <div className="w-full text-base font-medium">Pkwu</div>
+                <div className="w-full text-base font-medium">B.indonesia</div>
             </div>
 
             <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-                <div className="w-full text-base font-medium">Geografi</div>
+                <div className="w-full text-base font-medium">Ipa</div>
             </div>
         </>
     )
