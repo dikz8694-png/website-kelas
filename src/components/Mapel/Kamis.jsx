844097c5bf3b426b71c3de03d9228c5814bafe6e
dikz8694-png <dikz8694@gmail.com>
2025-08-01 20:33:53 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -2,23 +2,23 @@ const Kamis = () => {
   return (
     <>
       <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-        <div className="w-full text-base font-medium">B.Inggris LM</div>
+        <div className="w-full text-base font-medium">Sbk</div>
       </div>
 
       <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-        <div className="w-full text-base font-medium">Ekonomi</div>
+        <div className="w-full text-base font-medium">B.Inggris</div>
       </div>
 
       <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-        <div className="w-full text-base font-medium">Sosiologi</div>
+        <div className="w-full text-base font-medium">B.Arab</div>
       </div>
 
       <div className="flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="800">
-        <div className="w-full text-base font-medium">B.Indonesia</div>
+        <div className="w-full text-base font-medium">-</div>
       </div>
 
       <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="900">
-        <div className="w-full text-base font-medium">Sejarah Peminatan</div>
+        <div className="w-full text-base font-medium">-</div>
       </div>
     </>
   );
