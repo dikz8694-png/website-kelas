f080182f4393c4b2e6c1418f4f069f2e0f193da1
dikz8694-png <dikz8694@gmail.com>
2025-08-01 20:36:35 +0700

    Update Sabtu.jsx

--- src/components/Mapel/Sabtu.jsx
+++ src/components/Mapel/Sabtu.jsx
@@ -2,11 +2,11 @@ const Sabtu = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Pkwu</div>
+					<div className="w-[50%] text-base  font-medium">Libur</div>
 					<div className="flex justify-center items-center text-sm">08.00-09.20</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Sejarah Peminatan</div>
+					<div className="w-[50%] text-base  font-medium">Libur</div>
 					<div className="flex justify-center items-center text-sm">09.20-10.00</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,12 +14,12 @@ const Sabtu = () => {
 					<div className="flex justify-center items-center text-sm">10.00-10.15</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
+					<div className="w-[50%] text-base  font-medium">Libur</div>
 					<div className="flex justify-center items-center text-sm">10.15-10.55</div>
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Seni Budaya</div>
+					<div className="w-[50%] text-base  font-medium">Libur</div>
 					<div className="flex justify-center items-center text-sm">10.55-12.15</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
@@ -28,7 +28,7 @@ const Sabtu = () => {
 				</div>
 				
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="1100">
-					<div className="w-[50%] text-base  font-medium">PKN</div>
+					<div className="w-[50%] text-base  font-medium">Libur</div>
 					<div className="flex justify-center items-center text-sm">12.45-14.05</div>
 				</div>
         </>
